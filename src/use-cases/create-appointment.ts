03770245f3def50be17a2f4f15d0c7bf3645e686
Cayke Daniel <caykedanielcd@gmail.com>
2022-09-27 15:09:48 -0300

    Appointments are now unable to overlap, using memory to 'simulate a database' and persist data

--- src/use-cases/create-appointment.ts
+++ src/use-cases/create-appointment.ts
@@ -1,4 +1,5 @@
 import { Appointment } from "../entities/appointment";
+import { AppointmentsRepository } from "../repositories/appointments-repository";
 
 interface CreateAppointmentRequest {
     customer: string;
@@ -7,12 +8,27 @@ interface CreateAppointmentRequest {
 }
 type CreateAppointmentResponse = Appointment;  
 export class CreateAppointment {
+    constructor(
+        private appointmentsRepository: AppointmentsRepository
+    ) {}
     async execute({customer, startsAt, endsAt}: CreateAppointmentRequest): Promise<CreateAppointmentResponse> {
+        const overlappingAppointment = await this.appointmentsRepository.findOverlappingAppointment(
+            startsAt, 
+            endsAt
+        )
+
+        if(overlappingAppointment) {
+            throw new Error("Another appointment overlaps this appointment dates");
+            
+        }
+
         const appointment = new Appointment({
             customer,
             startsAt,
             endsAt
         })
+
+        await this.appointmentsRepository.create(appointment)
 
         return appointment;
     }
